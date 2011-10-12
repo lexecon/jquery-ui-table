5f6afcdf54486acd7b53d83279da117e227069d0
Alexey Krylov <lexecon117@gmail.com>
2011-10-12 14:48:02 +0400

    added functionality

--- jquery-ui.table.js
+++ jquery-ui.table.js
@@ -31,7 +31,13 @@
             positionListing:'br',
             positionSelected: 'bl',
             positionButtonBox: 'tr',
+            positionLabel:'',
             tableHead: '',
+            scroll:'x',
+            staticHead: false,
+            widthTableContent:'auto',
+            heightTableContent:'auto',
+            labelText:'default text',
             tableCurentPage: 0,
             tableRowsOnPage: 20,
             tableWidthColumns: [],
@@ -39,8 +45,16 @@
             tableRowsOnPageMax:100,
             tableStepInterval: 10,
             visibleRowsOnPage: true,
+            highlighting: false,
             getCellData:function(x, y, value){
                 return '<td class="tableUiData">'+value+'</td>';
+            },
+            getSelectedHtml:function(countRows, startRows, endRows){
+                if (countRows == 0){
+                    return  '<label><span>Rows selected: 0</span></label>';
+                }else{
+                    return '<label><span>Selected  '+startRows+' - '+endRows+'  of '+countRows+'</span></label>';
+                }
             },
             buttons:{}
         },options);
@@ -57,21 +71,35 @@
             structureObject += '<div class="fg-toolbar ui-toolbar ui-widget-header ui-corner-tl ui-corner-tr ui-helper-clearfix" style="font-size:14px;padding:2px 5px 2px 5px;min-height:2px;overflow:hidden">';
             structureObject +='<div class="tlTemplate" style="float:left;overflow:hidden;height:2px"></div>';
             structureObject +='<div class="trTemplate" style="float:right;overflow:hidden; height:2px"></div>';
-            structureObject +='</div><div class="tableContent"><div><table style=\"width:100%;\"></table></div></div>';
+            structureObject +='<div class="tcTemplate" style="overflow:hidden;height:0px;text-align:center;width:auto"></div>';
+            structureObject +='</div><div class="tableHead"></div><div class="tableContent" style="width:'+options.widthTableContent+';height:'+options.heightTableContent+'"><div><table style=\"width:100%;\"></table></div></div>';
             structureObject +='<div class="fg-toolbar ui-toolbar ui-widget-header ui-corner-bl ui-corner-br ui-helper-clearfix" style="font-size:14px;padding:2px 5px 2px 5px">';
             structureObject +='<div class="blTemplate" style="float:left;overflow:hidden;height:2px"></div>';
             structureObject +='<div class="brTemplate" style="float:right;overflow:hidden;height:2px"></div>';
+            structureObject +='<div class="bcTemplate" style="overflow:hidden;height:0px;text-align:center;width:auto"></div>';
             structureObject +='</div></div>';
             element.html(structureObject);
             var colorBorder = element.children('div').children('.ui-widget-header').css('border-left-color');
             element.children('div').children('.tableContent').css('border-left','1px solid '+colorBorder);
             element.children('div').children('.tableContent').css('border-right','1px solid '+colorBorder);
-            var cssScrollX = {
-                width:'100%',
-                overflowX:'auto', 
-                overflowY:'hidden'
-            };
-            element.children('div').children('.tableContent').children('div').css(cssScrollX);
+            element.children('div').children('.tableHead').css('border-left','1px solid '+colorBorder);
+            element.children('div').children('.tableHead').css('border-right','1px solid '+colorBorder);
+            if (options.scroll == 'x'){
+                var cssScroll = {
+                    width:'100%',
+                    height:'100%',
+                    overflowX:'auto', 
+                    overflowY:'hidden'
+                };
+            }else{
+                var cssScroll = {
+                    width:'100%',
+                    height:'100%',
+                    overflowX:'hidden', 
+                    overflowY:'auto'
+                };
+            }
+            element.children('div').children('.tableContent').children('div').css(cssScroll);
             this.init = function(){
                 if (!$.isEmptyObject(options.JSONHead)){
                     if (options.JSONData.length%options.JSONHead.length == 0){
@@ -111,8 +139,14 @@
         TemplateClass.prototype.getTable = function(){
             return this.element.children('div').children('.tableContent').children('div').children('table');
         }
+        TemplateClass.prototype.getTableHead = function(){
+            return this.element.children('div').children('.tableHead');
+        }
         TemplateClass.prototype.getCountRows = function(){
             return globarCountRows;
+        }
+        TemplateClass.prototype.setHeightTableContent = function(height){
+            this.element.children('div').children('.tableContent').css('height',height)
         }
         //  ------------------------------------------------------------------------
         // Create class TableRowsOnPage --------------------------------------------
@@ -295,14 +329,8 @@
                 if (this.getCountRows()< num){
                     num = this.getCountRows();
                 }
-                var html ='';
-                if (this.getCountRows() == 0){
-                    html = '<label><span>Rows selected: 0</span></label>';
-                }else{
-                    html = '<label><span>Selected  '+((this._curPage)*this._rowsOnPage+1)+' - '+num+'  of '+this.getCountRows()+'</span></label>';
-                }
                 this._element.css('height','auto');
-                this._element.html(html);
+                this._element.html(options.getSelectedHtml(this.getCountRows(),((this._curPage)*this._rowsOnPage+1),num));
                 this.OnChange();
             }
             this.onChange = function(sender){
@@ -312,6 +340,17 @@
             }
         }
         TableSelected.prototype = new TemplateClassDo();
+        //   -----------------------------------------------------------------------
+        //   Create class TableLabel --------------------------------------------
+        var TableLabel = function(){
+            this.arrayListener=[];
+            this._element = this.getPartTemplate(options.positionLabel);
+            this.onChange = function(sender){
+                this._element.css('height','auto');
+                this._element.html('<span>'+options.labelText+'</span>');
+            }
+        }
+        TableLabel.prototype = new TemplateClassDo();
         //   -----------------------------------------------------------------------
         //   Create class TableBox -------------------------------------------------
         var TableBox = function (){
@@ -354,6 +393,13 @@
                 table += "</table>";
                 this._element.replaceWith(table);
                 this._element = this.getTable();
+                if (options.highlighting){
+                    $('tr td',this._element).not('.tableUiHead').hover(function(){
+                        $(this).parent('tr').children('td').addClass('rowHower');
+                    }, function(){
+                        $(this).parent('tr').children('td').removeClass('rowHower');
+                    });
+                }
                 if ($('td',$('tr',this._element).last()).size() == options.tableWidthColumns.length){
                     var widthTable = 0;
                     for (var w = 0;w < options.tableWidthColumns.length; w++){
@@ -362,19 +408,41 @@
                     }
                     this._element.css('width',widthTable+'px');
                 }
-                this._element.css('display','table');
                 // IE bug fix -----
                 var ua = navigator.userAgent.toLowerCase();
                 if (ua.indexOf("msie") != -1 && ua.indexOf("opera") == -1 && ua.indexOf("webtv") == -1) {
                     this._element.css('display','block');
                     var scrollHeight = this._element.parent('div').get(0).offsetHeight - this._element.parent('div').get(0).clientHeight;
+                    var scrollWidth = this._element.parent('div').get(0).offsetWidth - this._element.parent('div').get(0).clientWidth;
+                    if (scrollWidth != 0){
+                        this._element.css('width',(parseInt(this._element.css('width'),10)-scrollWidth)+'px');
+                        this._element.css('margin-right',scrollWidth + 'px');
+                    }
                     this._element.css('margin-bottom',scrollHeight + 'px');
+                }else {
+                    this._element.css('display','table');
                 }
-            // ----------------
+                                
+                // ----------------
+                if (options.scroll == 'y' && options.staticHead){
+                    var heightHead = $('td',$('tr',this._element).eq(0)).eq(0).get(0).offsetHeight;
+                    this._element.css('margin-top','-'+heightHead+'px');
+                    this.getTableHead().css('height',heightHead+'px');
+                    this.getTableHead().html('<table border=1 cellspacing=0 cellpadding=1><tr>'+$('tr',this._element).eq(0).html()+'</tr></table>')
+                    for (var r = 0;r<$('td',$('tr',this._element).eq(0)).size();r++){
+                        $('tr td',this.getTableHead().children('table')).eq(r).css('width',$('td',$('tr',this._element).eq(0)).eq(r).get(0).clientWidth);
+                    }
+                }
             }
             this.onChange = function(sender){
-                this._curPage = sender.getCurentPage();
-                this._rowsOnPage = sender.getRowsOnPage();
+                this._curPage = this.getCurentPage();
+                this._rowsOnPage = this.getRowsOnPage();
+                this.update();
+            }
+            this.updateTable = function(Data, rowsOnPage){
+                this._Data = Data;
+                this._curPage = 0;
+                this._rowsOnPage = rowsOnPage;
                 this.update();
             }
         }
@@ -431,20 +499,20 @@
         var tableSelected = new TableSelected();
         var tableBox = new TableBox();
         var buttonBox = new ButtonBox();
+        var tableLabel = new TableLabel();
         this.getWidget = function(){
-                return widget;
-            }
+            return widget;
+        }
+        this.updateTable= function(Data, rowsOnPage){
+            tableBox.updateTable(Data, rowsOnPage);
+        }
         this.each(function() {
         
-            widget.addArrayListener([rowsOnPage, buttonBox]);
+            widget.addArrayListener([rowsOnPage, buttonBox, tableLabel]);
             rowsOnPage.addArrayListener([tableListing]);
             tableListing.addArrayListener([tableSelected]);
             tableSelected.addArrayListener([tableBox]);
             widget.init();
-        
-        /*if (!$.isEmptyObject(options.JSONHead)){
-            var buttonBox = new ButtonBox(widget);
-        }*/
         });
         return this;
     }
